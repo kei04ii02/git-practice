67a11b6e0e0f17059e7c35c8db77b75b549640a7
kei04ii02 <116486994+kei04ii02@users.noreply.github.com>
2022-10-26 13:28:36 +0900

    Add files via upload

---
+++ practice/alert.js
@@ -0,0 +1,14 @@
+function loadHello(){
+  alert("Hello!");
+}
+function loadHello(){
+    alert("Hello!");
+  }
+  
+  function sum(){
+    let x = 3;
+    let y = 4;
+    let z = x + y;
+    alert(z);
+  }
+  
